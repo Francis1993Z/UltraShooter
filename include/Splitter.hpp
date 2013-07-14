223abb318ed2f70cf9b0758828b666afe943359c
Francis1993Z <franky_1993z@live.ca>
2013-07-14 11:31:37 -0400

    Ajout des Splitters.

---
+++ include/Splitter.hpp
@@ -0,0 +1,15 @@
+#ifndef SPLITTER_HPP_INCLUDED
+#define SPLITTER_HPP_INCLUDED
+
+#include "Zombie.hpp"
+
+class Splitter : public Zombie
+{
+public:
+    Splitter(sf::Vector2f init_Position, Player& init_Target, unsigned int level);
+    unsigned int getLevel() const;
+private:
+unsigned int my_level;
+};
+
+#endif // SPLITTER_HPP_INCLUDED
---
+++ src/Splitter.cpp
@@ -0,0 +1,53 @@
+#include "Splitter.hpp"
+
+
+Splitter::Splitter(sf::Vector2f init_Position, Player& init_Target, unsigned int level)
+{
+my_level=level;
+    float radius=100;
+    float e_m=6;
+    float n_radius=radius;
+kill_point=5;
+    if (level==2)
+        {
+            n_radius=radius/2;
+kill_point=kill_point*2;
+            e_mx=e_m*2;
+            e_my=e_m*2;
+        }
+
+    else if (level==3)
+        {
+            n_radius=radius/4;
+kill_point=kill_point*4;
+            e_mx=e_m*4;
+            e_my=e_m*4;
+        }
+
+    else if (level==4)
+        {
+            n_radius=radius/8;
+kill_point=kill_point*8;
+            e_mx=e_m*8;
+            e_my=e_m*8;
+        }
+
+
+
+    Splitter::setRadius(n_radius);
+    Splitter::setOrigin(sf::Vector2f(n_radius/2, n_radius/2));
+    Splitter::setPosition(init_Position);
+    Splitter::setFillColor(sf::Color::Green);
+
+
+    d_radius=Splitter::getRadius();
+    my_behaviour=ATTACK;
+    my_target=&init_Target;
+}
+
+
+unsigned int Splitter::getLevel() const
+{
+return my_level+1;
+}
+
